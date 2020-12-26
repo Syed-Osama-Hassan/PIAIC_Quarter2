fd7741a275fa29768f7ca7c51238fdc5e800d131
Syed-Osama-Hassan <48532051+Syed-Osama-Hassan@users.noreply.github.com>
2020-12-27 02:22:23 +0500

    Task 10

---
+++ task10.js
@@ -0,0 +1,17 @@
+var x;
+document.write("Value after variable declaration is: " + x +"<br>");
+
+x = 10;
+document.write("Initial value: " + x+"<br>");
+
+x++;
+document.write("Value after increment is: " + x+"<br>");
+
+x += 7;
+document.write("Value after addition is: " + x +"<br>");
+
+x--;
+document.write("Value after decrement is: " + x + "<br>");
+
+x = x % 3;
+document.write("The remainder is: " + x + "<br>");
