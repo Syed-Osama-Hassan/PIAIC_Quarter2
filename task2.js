db8c7153a680969b1f759e28d7b5552ad75e25ef
Syed-Osama-Hassan <48532051+Syed-Osama-Hassan@users.noreply.github.com>
2020-12-27 01:44:34 +0500

    Task 2 Updated and Task 3

--- task2.js
+++ task2.js
@@ -1,6 +1,6 @@
-fullName = "Jhone Doe";
-age = "15 years old";
-bio = "Certified Mobile Application Development";
+var fullName = "Jhone Doe";
+var age = "15 years old";
+var bio = "Certified Mobile Application Development";
 
 alert(fullName);
 alert(age);
---
+++ task3.js
@@ -0,0 +1,3 @@
+var email = "user@example.com";
+
+alert("My email address is " + email);
