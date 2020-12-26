9469b6f85b68e24e7d9e6298cf43c650e1fa1e72
Syed-Osama-Hassan <48532051+Syed-Osama-Hassan@users.noreply.github.com>
2020-12-27 02:14:33 +0500

    Task 9

---
+++ task9.js
@@ -0,0 +1,14 @@
+var a = 3;
+var b = 5;
+
+var c = a + b;
+var d = a - b;
+var e = a * b;
+var f = a / b;
+var g = a % b;
+
+document.write("Sum of " + a + " and " + b + " is " + c +"<br>");
+document.write("Subtraction of " + a + " and " + b + " is " + d+"<br>");
+document.write("Multiplication of " + a + " and " + b + " is " + e+"<br>");
+document.write("Division of " + a + " and " + b + " is " + f+"<br>");
+document.write("Modulus of " + a + " and " + b + " is " + g+"<br>");
