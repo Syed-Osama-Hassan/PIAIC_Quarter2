a79f56f90360a2aa06174144440c526fec0c09ab
Syed-Osama-Hassan <48532051+Syed-Osama-Hassan@users.noreply.github.com>
2021-01-04 02:39:59 +0500

    Task 6

---
+++ assignment2_PIAIC52128/task6.js
@@ -0,0 +1,11 @@
+var number = prompt("Enter a number");
+var answer = parseInt(number) % 2;
+
+if(answer == 0)
+{
+    alert("You have entered an even number");
+}
+else
+{
+    alert("You have entered an odd number");
+}
