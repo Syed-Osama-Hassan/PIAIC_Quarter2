901cb22b125020bfc332bd95d35dbb4a2e500805
Syed-Osama-Hassan <48532051+Syed-Osama-Hassan@users.noreply.github.com>
2021-01-04 02:59:14 +0500

    Task 9

---
+++ assignment2_PIAIC52128/task9.js
@@ -0,0 +1,12 @@
+var character = prompt("Enter a single character");
+
+if(character.localeCompare("a") == 0 || character.localeCompare("e") == 0
+ || character.localeCompare("i") == 0 || character.localeCompare("o") == 0
+ || character.localeCompare("u") == 0)
+ {
+    alert("True");
+ }
+ else
+ {
+    alert("False");
+ }
