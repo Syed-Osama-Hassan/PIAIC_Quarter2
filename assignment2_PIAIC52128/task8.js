721fa29b64737e44d81411247d7a19e3ee120c7f
Syed-Osama-Hassan <48532051+Syed-Osama-Hassan@users.noreply.github.com>
2021-01-04 02:53:49 +0500

    Task 8

---
+++ assignment2_PIAIC52128/task8.js
@@ -0,0 +1,14 @@
+var number = Number(prompt("Enter a number"));
+
+if( number > 0)
+{
+    alert("Positive Number");
+}
+else if(number === 0)
+{
+    alert("Zero");
+}
+else if(number < 0)
+{
+    alert("Negative Number");
+}
