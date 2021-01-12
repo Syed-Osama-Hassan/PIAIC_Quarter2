6bb74fa0d73268b303573acea8bf549419d24d4b
Syed-Osama-Hassan <48532051+Syed-Osama-Hassan@users.noreply.github.com>
2021-01-13 01:52:18 +0500

    Task 1

---
+++ assignment3_PIAIC52128/task1.js
@@ -0,0 +1,8 @@
+var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"];
+
+document.write("<h1>Qualifications:</h1> <br><br>");
+
+for(var i = 0; i < qualifications.length; i++)
+{
+    document.write(i + 1 + ") "+qualifications[i] + "<br>");
+}
