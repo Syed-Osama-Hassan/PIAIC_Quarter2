a00a3157276081385af089b9935d6bfd1892d46f
Syed-Osama-Hassan <48532051+Syed-Osama-Hassan@users.noreply.github.com>
2021-01-13 02:38:30 +0500

    Task 4

---
+++ assignment3_PIAIC52128/Task4.js
@@ -0,0 +1,9 @@
+var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+document.write("Cities list: <br>");
+document.write(cities + "<br><br>");
+
+var selectedCities = [];
+selectedCities[0] = cities[2];
+selectedCities[1] = cities[3];
+document.write("Selected Cities list: <br>");
+document.write(selectedCities);
