efc900fb837526e0353547d797dbee555ea017d3
Syed-Osama-Hassan <48532051+Syed-Osama-Hassan@users.noreply.github.com>
2021-01-18 12:48:11 +0500

    Task2

---
+++ assignment4_PIAIC52128/task2.js
@@ -0,0 +1,7 @@
+var value = "472";
+document.write("Value:", value + "<br>");
+document.write("Type:", typeof(value) + "<br>");
+
+var valueNum = Number(value);
+document.write("Value:", valueNum + "<br>");
+document.write("Type:", typeof(valueNum) + "<br>");
