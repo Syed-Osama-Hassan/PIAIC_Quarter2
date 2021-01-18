5020a92a791b57bd3fefac5c3369d62b257fecbe
Syed-Osama-Hassan <48532051+Syed-Osama-Hassan@users.noreply.github.com>
2021-01-18 12:43:14 +0500

    Task1

---
+++ assignment4_PIAIC52128/task1.js
@@ -0,0 +1,7 @@
+//Before replacement
+var city = "Hyderabad";
+document.write("City: " + city + "<br>");
+
+//After replacement
+var replacement = city.replace("Hyder","Islam");
+document.write("After replacement: " + replacement);
